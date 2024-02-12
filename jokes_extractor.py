6d4db8156b3dd6f67c01655443f52a737927ff00
AngeGitego <144104486+AngeGitego@users.noreply.github.com>
2024-02-12 10:49:04 -0800

    Update jokes_extractor.py

--- jokes_extractor.py
+++ jokes_extractor.py
@@ -1,1 +1,8 @@
+import re
+
+def extract_jokes(response):
+    pattern = r"Why did the (.+?) \? Because(.+)"
+    matches = re.findall(pattern, response)
+    jokes = [(setup, punchline) for setup, punchline in matches]
+    return jokes
 
